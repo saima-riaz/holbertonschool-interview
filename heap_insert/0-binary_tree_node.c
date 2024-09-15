7c894dffc772d09fd29cb20a7b9993ede279b2c0
saima-riaz <saimariaz1422@gmail.com>
2024-09-15 21:27:52 +0200

    betty  code issue fixed

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -3,19 +3,17 @@
 
 /**
  * binary_tree_node - Creates a binary tree node
- *
  * @parent: Pointer to the parent node
  * @value: Value to put in the new node
  *
  * Return: Pointer to the new node, or NULL on failure
  */
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
 	new_node = malloc(sizeof(binary_tree_t));
-	if (!new_node)
+	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = value;
