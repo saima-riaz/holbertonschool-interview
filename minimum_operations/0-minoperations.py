02a326f9bbef2ad7861b53d05fd1c979c5b46074
saima-riaz <saimariaz1422@gmail.com>
2024-09-19 17:19:31 +0200

    fixed  phython code style

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""Module which contains function"""
+"""Module which contains function to calculate min operations."""
 
 
 def minOperations(n):
-    """Calculate the minimum number of
-    operations needed to get exactly n 'H' characters"""
+    """Calculate the minimum number of operations
+    needed to get exactly n 'H' characters."""
     if n <= 1:
         return 0
 
     i = 0
     x = 2  # Start dividing n by the smallest possible prime factors
-    
+
     while n > 1:
         while n % x == 0:
             i += x
