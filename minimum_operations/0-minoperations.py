4ad25c52e0592e66f8ef3411f8a7c5c8f267bb76
saima-riaz <saimariaz1422@gmail.com>
2024-09-19 17:04:28 +0200

    Calculate the minimum number of operations needed to get exactly n H characters

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,15 @@
+def minOperations(n):
+    """Calculate the minimum number of operations needed to get exactly n 'H' characters"""
+    if n <= 1:
+        return 0
+
+    i= 0
+    # Start dividing n by the smallest possible prime factors
+    x = 2
+    while n > 1:
+        while n % x == 0:
+            i += x
+            n //= x
+        x += 1
+
+    return i
