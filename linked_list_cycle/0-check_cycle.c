c5ed7db19eee5eea361e629d098c1d730d2bd949
saima-riaz <saimariaz1422@gmail.com>
2024-10-17 22:57:58 +0200

    Implement function to detect cycle in a singly linked list using Floyd's Tortoise and Hare algorithm

---
+++ linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle
+ * @list: pointer to the head of the list
+ * 
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+ int check_cycle(listint_t *list)
+ {
+    listint_t *tortoise = list;
+    listint_t *hare = list;
+
+    while (hare != NULL && hare->next != NULL)
+    {
+        tortoise = tortoise->next;
+        hare = hare->next->next;
+        
+        if (tortoise == hare)
+            return (1); /* Cycle detected */
+    }
+
+    return (0); /* no Cycle detected */
+ }
