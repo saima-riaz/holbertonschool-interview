904dd46fc80b705a49f5d9b314435d5522559f0a
saima-riaz <saimariaz1422@gmail.com>
2024-11-12 23:55:27 +0100

    fixed berrty issue

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -1,44 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "menger.h"
 
-
 /**
-*menger - draws a 2d Menger Sponge at a given level 
-* @level: Level of the Menger Sponge to draw
-*/
-
+ * menger - Draws a 2D Menger Sponge at a given level
+ * @level: Level of the Menger Sponge to draw
+ */
 void menger(int level)
 {
-    int n, m, j, x, y;
-    char s;
+	int n, m, j, x, y;
+	char s;
 
-
-    m =(int)pow(3, level); //// Cast to int to avoid floating-point issues
-    for (n = 0; n < m; n++)
-    {
-        for (j = 0; j < m;)
-        {
-            s = '#';
-            x = n;
-            y = j++;
-            while (x > 0 || y > 0)
-            {
-                if (x % 3 == 1 && y % 3 == 1)
-                {
-                    s = ' ';
-                    break; // Exit loop early once space is confirmed
-                }
-                x /= 3;
-                y /= 3;
-            }
-            putchar(s);// Use putchar instead of printf for efficiency
-            
-        }
-        putchar('\n');
-
-    }
+	m = (int)pow(3, level); /* Cast to int to avoid floating-point issues */
+	for (n = 0; n < m; n++)
+	{
+		for (j = 0; j < m;)
+		{
+			s = '#';
+			x = n;
+			y = j++;
+			while (x > 0 || y > 0)
+			{
+				if (x % 3 == 1 && y % 3 == 1)
+				{
+					s = ' ';
+					break; /* Exit loop early once space is confirmed */
+				}
+				x /= 3;
+				y /= 3;
+			}
+			putchar(s); /* Use putchar instead of printf for efficiency */
+		}
+		putchar('\n');
+	}
 }
-
-
-
