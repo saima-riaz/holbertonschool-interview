07864bec19d2179372afd30a7bcc0ad54e0c531e
saima-riaz <saimariaz1422@gmail.com>
2024-11-18 15:44:22 +0100

    fix error

--- slide_line/0-slide_line.c
+++ slide_line/0-slide_line.c
@@ -10,46 +10,65 @@
  */
 int slide_line(int *line, size_t size, int direction)
 {
-    size_t i, j, start, step;
-    int merged[size];
+    size_t i, insert_pos;
+    int temp[size];
 
     if (direction != SLIDE_LEFT && direction != SLIDE_RIGHT)
         return (0);
-    for (i = 0; i < size; i++)
-        merged[i] = 0;
 
+    /* Initialize the temporary array */
+    for (i = 0; i < size; i++)
+        temp[i] = 0;
+
+    /* Slide and merge logic */
     if (direction == SLIDE_LEFT)
     {
-        start = 0;
-        step = 1;
-    }
-    else
-    {
-        start = size - 1;
-        step = -1;
-    }
-
-    for (i = start; i < size; i += step)
-    {
-        if (line[i] == 0)
-            continue;
-
-        j = i;
-        while (j + step >= 0 && j + step < size && line[j + step] == 0)
+        insert_pos = 0;
+        for (i = 0; i < size; i++)
         {
-            line[j + step] = line[j];
-            line[j] = 0;
-            j += step;
-        }
-
-        if (j + step >= 0 && j + step < size &&
-            line[j + step] == line[j] && !merged[j + step])
-        {
-            line[j + step] += line[j];
-            line[j] = 0;
-            merged[j + step] = 1;
+            if (line[i] != 0)
+            {
+                if (temp[insert_pos] == 0)
+                    temp[insert_pos] = line[i];
+                else if (temp[insert_pos] == line[i])
+                {
+                    temp[insert_pos] += line[i];
+                    insert_pos++;
+                }
+                else
+                {
+                    insert_pos++;
+                    temp[insert_pos] = line[i];
+                }
+            }
         }
     }
+    else /* SLIDE_RIGHT */
+    {
+        insert_pos = size - 1;
+        for (i = size; i-- > 0;)
+        {
+            if (line[i] != 0)
+            {
+                if (temp[insert_pos] == 0)
+                    temp[insert_pos] = line[i];
+                else if (temp[insert_pos] == line[i])
+                {
+                    temp[insert_pos] += line[i];
+                    insert_pos--;
+                }
+                else
+                {
+                    insert_pos--;
+                    temp[insert_pos] = line[i];
+                }
+            }
+        }
+    }
+
+    /* Copy the result back to the original line array */
+    for (i = 0; i < size; i++)
+        line[i] = temp[i];
 
     return (1);
 }
