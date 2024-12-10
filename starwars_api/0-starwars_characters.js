e73137bc4068ea68159355214d242eba8a5e0b4b
saima-riaz <saimariaz1422@gmail.com>
2024-12-10 15:22:10 +0100

    fix error

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,43 +1,48 @@
-#!/opt/homebrew/bin/node
-
-const axios = require('axios');  // Use axios instead of request
-
-const movieId = process.argv[2];  // Get the Movie ID from the command line arguments
-
-// Check if movieId is provided
+#!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
 if (!movieId) {
-  console.log('Please provide a movie ID as a positional argument.');
+  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
   process.exit(1);
 }
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error fetching data:', error);
+    return;
+  }
 
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;  // Star Wars API endpoint for the movie
+  if (response.statusCode !== 200) {
+    console.error(`Error: Received status code ${response.statusCode}`);
+    return;
+  }
 
-// Make a request to the API to fetch movie details
-axios.get(url)
-  .then((response) => {
-    const movieData = response.data;
-    const characters = movieData.characters;
-
-    // Create an array of promises for each character
-    const characterPromises = characters.map(characterUrl => {
-      return axios.get(characterUrl)
-        .then(response => response.data.name)  // Extract character name from response
-        .catch(error => console.error('Error fetching character data:', error));
+  const filmData = JSON.parse(body);
+  const characters = filmData.characters;
+  const fetchCharacterName = (url, callback) => {
+    request(url, (err, res, charBody) => {
+      if (err) {
+        callback(err, null);
+        return;
+      }
+      const charData = JSON.parse(charBody);
+      callback(null, charData.name);
     });
+  };
 
-    // Wait for all promises to resolve and print character names in order
-    Promise.all(characterPromises)
-      .then(characterNames => {
-        characterNames.forEach(name => {
-          if (name) {
-            console.log(name);  // Print the character's name
-          }
-        });
-      })
-      .catch((error) => {
-        console.error('Error fetching character data:', error);
-      });
-  })
-  .catch((error) => {
-    console.error('Error fetching movie data:', error);
-  });
+  const printCharacters = (index) => {
+    if (index >= characters.length) {
+      return;
+    }
+    fetchCharacterName(characters[index], (err, name) => {
+      if (err) {
+        console.error('Error fetching character:', err);
+      } else {
+        console.log(name);
+        printCharacters(index + 1);
+      }
+    });
+  };
+
+  printCharacters(0);
+});
