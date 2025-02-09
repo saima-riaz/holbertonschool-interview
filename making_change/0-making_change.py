2b893e75150dd83b7e0902b70339f76f78d8124c
saima-riaz <saimariaz1422@gmail.com>
2025-02-10 00:38:31 +0100

    minimum number of coins needed to make up the total

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -8,12 +8,14 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
     
-    coins.sort(reverse=True)  # Start with the largest coin
+    coins.sort(reverse=True)
     count = 0
     
     for coin in coins:
-        while total >= coin:
-            total -= coin
-            count += 1
+        if total == 0:
+            break
+        num_coins = total // coin
+        count += num_coins
+        total -= num_coins * coin
     
     return count if total == 0 else -1
