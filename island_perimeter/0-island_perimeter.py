e0c756e7be3a4f4fea8abbaa4f3366b52150c5fb
saima-riaz <saimariaz1422@gmail.com>
2025-03-03 16:34:02 +0100

    fixed error

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""
+Module for calculating the perimeter of an island in a grid.
+"""
 
 def island_perimeter(grid):
-    """Returns the perimeter of the island described in grid."""
+    """Returns the perimeter of the island described in grid"""
     perimeter = 0
     rows, cols = len(grid), len(grid[0])
 
@@ -9,8 +12,8 @@ def island_perimeter(grid):
         for c in range(cols):
             if grid[r][c] == 1:
                 perimeter += 4
-                if r > 0 and grid[r - 1][c] == 1:
+                if r > 0 and grid[r - 1][c] == 1:  # Check top
                     perimeter -= 2
-                if c > 0 and grid[r][c - 1] == 1:
+                if c > 0 and grid[r][c - 1] == 1:  # Check left
                     perimeter -= 2
     return perimeter
